aec1b11e877deb0285fba9d5ca0f8c0fe54d7edf
Charlie Knight <williamchknight@gmail.com>
2020-10-29 17:11:07 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,7 @@
 # Write your code here
-count = 10 
-new_year = 0 
-while count > 0
- puts count
- count -= 1
+count = 10
+while count >= 1 do
+  puts count
+  count -= 1
 end
+puts "Happy New Year!"
